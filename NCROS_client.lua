afb225701c55c84bf82fd18ac2167c9e524850e4
bcuebler <74453403+bcuebler@users.noreply.github.com>
2025-05-04 09:32:31 +0200

    Update NCROS_client.lua

--- NCROS_client.lua
+++ NCROS_client.lua
@@ -11,14 +11,8 @@ modem.open(port)
  screen = component.list("screen")()
  component.invoke(gpu, "bind", screen)
  component.invoke(gpu, "setResolution", 50, 16)
- component.invoke(gpu, "fill", 1, 1, 50, 16, " ")
+ term.clear()
 while true do
  local _, _, _, _, _, message = event.pull("modem_message")
-  local x = 1
-  local y = 1
-  component.invoke(gpu, "fill", 1, 1, 50, 16, " ")
- for line in string.gmatch(message, "([^\n]+)") do
-  component.invoke(gpu, "set", x, y, line)
-  y = y + 1
- end
+print(tostring(message))
 end
