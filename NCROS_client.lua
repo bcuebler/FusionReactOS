e54bb6b435c83632b476d7d3794907ed91e2ca56
bcuebler <74453403+bcuebler@users.noreply.github.com>
2025-05-05 17:28:36 +0200

    Update NCROS_client.lua

--- NCROS_client.lua
+++ NCROS_client.lua
@@ -7,17 +7,37 @@
 --tier 1 cpu/apu or more
 
 local component = require("component")
+local gpu = component.proxy(component.list("gpu")())
+local screen = component.proxy(component.list("screen")())
+local modem = component.proxy(component.list("modem")())
 local event = require("event")
 local term = require("term")
-local modem = component.modem
 
-term.clear()
-print("Modem port?")
-port = tonumber(io.read())
-modem.open(port)
-term.clear()
+gpu.bind(screen.address)
+gpu.setResolution(50, 10)
+--gpu.setForeground(0xFFFFFF)
+--gpu.setBackground(0x000000)
+
+gpu.fill(1, 1, 50, 10, " ")
+
+gpu.set(1, 1, "Modem port?")
+
+port = io.read()
+
+modem.open(tonumber(port))
+
+gpu.fill(1, 1, 50, 10, " ")
+
 while true do
- local _, _, _, _, _, message = event.pull("modem_message")
- term.clear()
- print(tostring(message))
+  local sig, _, _, _, _, msg = event.pull("modem_message")
+  if msg then
+    gpu.fill(1, 1, 50, 10, " ")
+
+    local i = 1
+    for line in tostring(msg):gmatch("([^\n]*)\n?") do
+      if i > 10 then break end
+      gpu.set(1, i, line:sub(1, 50))
+      i = i + 1
+    end
+  end
 end
