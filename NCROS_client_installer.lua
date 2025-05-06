ea2d83e4cbd4c1406d0610dd32a679ffb5ae8f3e
bcuebler <74453403+bcuebler@users.noreply.github.com>
2025-05-06 19:32:24 +0200

    Update NCROS_client_installer.lua

--- NCROS_client_installer.lua
+++ NCROS_client_installer.lua
@@ -16,8 +16,20 @@ print("MultiReactOS client Setup V1.0")
 print(" ")
 print("Insert an EEPROM to be written THEN continue")
 print(" ")
-print("Modem port?")
+print("Predefined modem port? [Y/n]")
+ pport = string.lower(tostring(io.read()))
+  if( pport == "y" ) then
+  pport = 1
+ else
+  pport = 0
+ end
+if( pport == 1 ) then
+ term.clear()
+ print("MultiReactOS client Setup V1.0")
+ print(" ")
+ print("Modem port?")
  port = tonumber(io.read())
+end
 term.clear()
 print("MultiReactOS client Setup V1.0")
 print(" ")
@@ -34,17 +46,50 @@ print(" ")
 
 --Main script
 script = [==[
+ pport = ]==] .. pport .. [==[
  port = ]==] .. port .. [==[
+local component = require("component")
 local gpu = component.proxy(component.list("gpu")())
 local screen = component.proxy(component.list("screen")())
 local modem = component.proxy(component.list("modem")())
+local event = require("event")
+local keyboard = component.proxy(component.list("keyboard")())
+
+local input = ""
 
 gpu.bind(screen.address)
 gpu.setResolution(50, 10)
 --gpu.setForeground(0xFFFFFF)
 --gpu.setBackground(0x000000)
 
+gpu.fill(1, 1, 50, 10, " ")
+
+if( pport == 0 )
+gpu.set(1, 1, "Modem port?")
+
+ while true do
+   local signalType, _, char, code = event.pull()
+
+   if signalType == "key_down" then
+     local c = string.char(char)
+    
+     if c:match("%d") then
+       input = input .. c
+       gpu.set(1, 2, input)
+     elseif char == 13 then
+       break
+     elseif char == 8 then
+       input = input:sub(1, -2)
+       gpu.set(1, 2, input .. " ")
+     end
+   end
+ end
+ port = tonumber(input)
+end
+
 modem.open(tonumber(port))
+
+gpu.fill(1, 1, 50, 10, " ")
 
 local sig, _, _, _, _, msg = event.pull("modem_message")
 local _, count = msg:gsub("\n", "")
