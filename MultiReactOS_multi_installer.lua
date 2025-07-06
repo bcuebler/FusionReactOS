e3840cd72dc536c6b5e7ec25d90892839f305411
bcuebler <74453403+bcuebler@users.noreply.github.com>
2025-07-06 16:25:30 +0200

    Update MultiReactOS_multi_installer.lua

--- MultiReactOS_multi_installer.lua
+++ MultiReactOS_multi_installer.lua
@@ -62,7 +62,7 @@ term.clear()
    end
  else
   fs = require("filesystem")
-  print("Installation path and name? E.g.: /home/MROS.lua")
+  print("Installation path? E.g.: /home")
   path = tostring(io.read())
   term.clear()
   print("MultiReactOS Setup V1.0")
